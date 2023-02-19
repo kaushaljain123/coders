3eaf071f9ead4f688a1ee2aef2e33be016baf52c
Bringonstore <84062175+myofflinemarket@users.noreply.github.com>
2023-02-19 20:38:22 +0530

    coders port update

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,6 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold))
